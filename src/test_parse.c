86fe21ccbd293a43861a8ab93fe9f8b469008737
Kieran OSullivan <whoiskieran@gmail.com>
2019-02-11 16:46:21 +0000

    Changes to be committed: 	modified:   src/Makefile 	new file:   src/Makefile~ 	deleted:    src/graph 	modified:   src/graph.h 	new file:   src/graph.h~ 	new file:   src/test_parse 	new file:   src/test_parse.c 	new file:   src/test_parse.c~

---
+++ src/test_parse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "graph.h"
+
+int main(int argc, char *argv[]) {
+  int no_e =0; int no_v=0;
+  char IfileName[1024];
+  char OfileName[1024];
+  
+  if(parse(argv, argc,&no_e,&no_v,IfileName,OfileName)==-1){return log_error("parse function returned -1");}
+  if(argc < 2){return log_error("function parse no arguments wrong");}
+  if(no_e ==0){return log_error("function parse no edges is  wrong");}
+  if(no_v ==0){return log_error("function parse no verticies is wrong");}
+      
+  return 0;
+}
