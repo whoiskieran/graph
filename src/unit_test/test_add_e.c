e86df5337b6b823e96db275addf1e5ce6bc6e131
Kieran OSullivan <whoiskieran@gmail.com>
2019-02-13 14:23:37 +0000

    Changes to be committed: 	modified:   src/Makefile 	deleted:    src/Makefile~ 	deleted:    src/graph.h~ 	modified:   src/unit_test/test_add_e.c

--- src/unit_test/test_add_e.c
+++ src/unit_test/test_add_e.c
@@ -8,39 +8,23 @@
 
 int main(int argc, char *argv[]) {
   graph G;
-  int no_e =10; int no_v=10;
+  int no_e =3; int no_v=3;
+  char output_str[512];  
+  if (init_G(&G,no_e,no_v)==-1){return log_error("function init_G failed to create graph.");}
   
-  if (init_G(&G,no_e,no_v)==-1){}
+  for (int i=0;i < no_v;i++){   
+    // The id of the vertix is irrelivent for this test.    
+    if (add_v(&G,i,i)==-1){return log_error("function add_v failed to add a vertix");}
+  }
+  // Create a triangel graph v0 connects to v1, v1 connects to v2, v2 connects to v0 
+  for (int i=0; i< no_v; i++){
+    sprintf(output_str,"function add_e failed to add edge=%d start_vertix=%d end_vertix=%d",i,i,(i+1)); 
+    if ((i+1)==no_v){
+       if (add_e(&G,i, i, 0, 2, 17, 17)==-1){return log_error(output_str);}
+    } else {
+      if (add_e(&G,i, i, (i+1), 2, 17, 17)==-1){return log_error(output_str);}
+    } // end if ((i+1)==no_v)   
+  }
   
-  if (add_v(&G,0,0)==-1){}
-  if (add_v(&G,1,1)==-1){}
-  if (add_v(&G,2,2)==-1){}
-  if (add_v(&G,3,3)==-1){}
-  if (add_v(&G,4,4)==-1){}
-  if (add_v(&G,5,5)==-1){}
-  
-  // Add connections
-  if (add_e(&G,0, 0, 1, 1, 0, 0)==-1){}
-  if (add_e(&G,1, 1, 2, 1, 0, 0)==-1){}
-  if (add_e(&G,2, 2, 3, 1, 0, 0)==-1){}
-  if (add_e(&G,3, 3, 4, 1, 0, 0)==-1){}
-  // if (add_e(&G,2, 2, 0, 2, 0, 0)==-1){}
-  // self referencing node.
-  // if (add_e(&G,3, 0, 0, 1, 0, 0)==-1){}
-  // if (add_e(&G,4, 1, 1, 1, 0, 0)==-1){}
-  // if (add_e(&G,5, 2, 2, 1, 0, 0)==-1){}
-  write_adjency(&G,' ',OfileName);
-  // if (move_e(&G, 3, 1, 1, 1, 0, 0)==-1){}
-  fprintf(stdout,"\nDegree of 2 is %d\n",get_degree(&G,2));
-  fprintf(stdout,"\nDegree of 5 is %d\n",get_degree(&G,5));
-  fprintf(stdout,"\nDegree of 4 is %d\n",get_degree(&G,4));
-  fprintf(stdout,"\nDistance 0,5=%d\n",v_distance(&G,0,5));
-  fprintf(stdout,"\nDistance 1,4=%d\n",v_distance(&G,1,4));
-  fprintf(stdout,"\nDistance 2,4=%d\n",v_distance(&G,2,4));
-  fprintf(stdout,"\nDistance 3,4=%d\n",v_distance(&G,3,4));
-  fprintf(stdout,"\nDistance 4,4=%d\n",v_distance(&G,4,4));
-  fprintf(stdout,"\nIsConnected = %d\n",G.is_connected);
-  fprintf(stdout, "\nGraph Created!\n");
-  write_incidence(&G,' ',OfileName);
   return 0;
 }
