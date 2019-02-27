68498f711e489bfd36aff805791b0e4cd2cb1aa8
Kieran OSullivan <whoiskieran@gmail.com>
2019-02-27 16:28:07 +0000

    Changes to be committed: 	modified:   src/Makefile 	modified:   src/Makefile~ 	new file:   src/unit_test/test_1_get_degree 	new file:   src/unit_test/test_2_get_degree.c

---
+++ src/unit_test/test_2_get_degree.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "graph.h"
+
+
+int main(int argc, char *argv[]) {
+  graph G;
+  int no_e =0; int no_v=0;
+  char IfileName[1024];
+  char OfileName[1024];
+  char msg[100];
+    
+  if (parse(argv, argc,&no_e,&no_v,IfileName,OfileName)==-1) {return log_error("function parse failed");}  
+  if (read_adjency(&G,' ',IfileName)==-1){return log_error("function failed read_adjacency");}
+  sprintf(msg,"read_adjency returned wrong number of edges. Edges is %ld it should be %d",get_no_e(&G),no_e);
+  if (get_no_e(&G) !=no_e){return log_error(msg);}
+  sprintf(msg,"read_adjency returned wrong number of verticies. Verticies is %ld it should be %d",get_no_v(&G),no_v);
+  if (get_no_v(&G) !=no_v){return log_error(msg);}
+  for (int i=0; i< no_v;i++){
+    if (get_degree(&G,i) !=2){return log_error("Degree incorrect");}
+  } 
+  return 0;
+}
